28c9ec3667a742a7e45c2cfcff013c51688ee5d8
Long <duclongt23@gmail.com>
2026-06-11 19:45:46 +0700

    doi style

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./style.css";
+import "./style2.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
